cb5280f77c5872a9244c014f13a94b1ba48c1607
Ivy <milai8899@gmail.com>
2025-05-06 01:14:31 +0800

    Menu 可動

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-add_action( 'wp_enqueue_scripts', function() {
+add_action('wp_enqueue_scripts', function () {
     // 父主題 CSS
     wp_enqueue_style(
         'blocksy-styles',
@@ -21,17 +21,21 @@ add_action( 'wp_enqueue_scripts', function() {
         'esim-child-style',
         get_stylesheet_directory_uri() . '/assets/css/main.css',
         ['blocksy-styles', 'google-fonts-noto-sans-tc'],
-        filemtime( get_stylesheet_directory() . '/assets/css/main.css' )
+        filemtime(get_stylesheet_directory() . '/assets/css/main.css')
     );
 
-    function load_custom_scripts() {
-        wp_enqueue_script(
-            'menu-toggle',
-            get_stylesheet_directory_uri() . '/assets/js/menu-toggle.js',
-            array(), // 如果需要 jQuery 就填 ['jquery']
-            false,
-            true // true 表示放在 body 結尾；false 表示放在 head
-        );
+    // 自己的 JS
+    wp_enqueue_script(
+        'main-js',
+        get_stylesheet_directory_uri() . '/assets/js/main.js',
+        [],
+        null,
+        true
+    );
+    add_filter('script_loader_tag', function ($tag, $handle, $src) {
+    if ($handle === 'menu-js') {
+        return "<script type='module' src='$src'></script>";
     }
-    add_action('wp_enqueue_scripts', 'load_custom_scripts');
+    return $tag;
+}, 10, 3);
 });
